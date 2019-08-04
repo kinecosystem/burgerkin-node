01cd6e886a2ad592278511b265cef15553087990
OrenZak <orenzak24@gmail.com>
2019-08-04 17:27:41 +0300

    add blockchain core

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = { master_public_address: 'GCUOZGHMN7XEGIDDCKDXWP732E2GKCYTBPSLY7PPJLDVBIBSKZUVPYTB',
+                   master_seed: 'SC2RWEIWGJNKULQXSGM5J37RP3N5U6LHDURIHE6NBG5X6XOSIHBMMTQS',
+                   appId: 'bkin' };
