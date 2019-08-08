4f81fc4a0f4293586c4630199f61df3c2ef2d888
ass <alon@bootleg.co.il>
2019-08-08 11:59:35 +0300

    a

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = { master_public_address: 'GCUOZGHMN7XEGIDDCKDXWP732E2GKCYTBPSLY7PPJLDVBIBSKZUVPYTB',
                    master_seed: 'SC2RWEIWGJNKULQXSGM5J37RP3N5U6LHDURIHE6NBG5X6XOSIHBMMTQS',
                    appId: 'bkin',
-                   board_width: 4,
-                   board_height: 5,
+                   board_width: 2,
+                   board_height: 2,
                    monitor_tables:true,
                    game_fee:10
                 };
